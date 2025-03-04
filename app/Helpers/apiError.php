16d083dd9ec31e4ce4cf4785dd7e6f2a61102133
sowidan1 <osamasowidan1@gmail.com>
2025-03-04 21:35:01 +0200

    update .env.example and make api helper response functions and includes in composer.json

---
+++ app/Helpers/apiError.php
@@ -0,0 +1,12 @@
+<?php
+
+function apiError(
+    $error,
+    $code,
+) {
+    $response = [
+        'success' => false,
+        'error' => $error,
+    ];
+    return response()->json($response, $code);
+}
